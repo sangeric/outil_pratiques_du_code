a575bc924aea23a146c7d64d39536daa5464df09
sangeric <eric.sang.pro@gmail.com>
2026-03-12 10:39:05 +0100

    eslint

---
+++ backend/app.js
@@ -0,0 +1,9 @@
+const x=10;
+int y =;
+console.log(x);
+
+function test(){
+console.log('test');
+}
+
+test();
---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,25 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  {
+    files: [
+      'src/**/*.{js,mjs,cjs}',
+    ],
+    plugins: { js },
+    languageOptions: {
+      globals: globals.node,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    rules: {
+      'indent': ['error', 2],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always'],
+      'no-console': 'warn',
+      'no-unused-vars': 'error',
+    },
+  },
+  js.configs.recommended,
+]);
